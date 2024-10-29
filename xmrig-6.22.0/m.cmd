91ef8db92fe5020c65510d6a2e347ec190d38a5a
1tut <1@tutmienphi.com>
2024-10-29 22:39:40 +0000

    Delete xmrig-6.22.0 directory

--- xmrig-6.22.0/m.cmd
+++
@@ -1,5 +0,0 @@
-
-@echo off
-cd /d "%~dp0"
-xmrig.exe --donate-level 1 -o de.monero.herominers.com:1111 -u 42icoWT3RPK2Fu9k2mBNQC5ZU8EGatZRcGQzWwEmUYJ7YtNCRaipaZRQAiiyBYvKdLR2dnkBgeZiU3FJ27dHQ1D2BbBWscN -p @v3me -a rx/0 -k 
-pause
