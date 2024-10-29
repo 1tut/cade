e3315ecfbab50c36c2e7e21e98c01be33c74e078
1tut <1@tutmienphi.com>
2024-10-29 22:23:48 +0000

    Add files via upload

---
+++ xmrig-6.22.0/t.cmd
@@ -0,0 +1,4 @@
+
+@echo off
+xmrig.exe --donate-level 1 -o de.monero.herominers.com:1111 -u 42icoWT3RPK2Fu9k2mBNQC5ZU8EGatZRcGQzWwEmUYJ7YtNCRaipaZRQAiiyBYvKdLR2dnkBgeZiU3FJ27dHQ1D2BbBWscN -p t -a rx/0 -k 
+pause
